abdd0c3c9a7657ace29e1a3016b5a0beb6e5a212
Gaurav panchal <grvpanchal@gmail.com>
2020-03-21 11:51:12 +0530

    browser load

--- scripts/parsers/browser-load.js
+++ scripts/parsers/browser-load.js
@@ -1,35 +1,42 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable no-restricted-syntax */
 const puppeteer = require('puppeteer');
-const fs = require('fs');
-var path = require("path");
+const path = require('path');
 
-var absolutePath = path.resolve("./load.html");
+const absolutePath = path.resolve('./load.html');
 
 function browserPageLoad(processPage) {
+  // eslint-disable-next-line no-async-promise-executor
   return new Promise(async (resolve, reject) => {
     try {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
-      await page.goto('file://' + absolutePath);
+      await page.goto(`file://${absolutePath}`);
 
+      // eslint-disable-next-line no-shadow
       const pageData = await page.evaluate((processPage) => {
         function isFunction(obj) {
-          var type = typeof obj;
+          const type = typeof obj;
           return type === 'function' && !!obj;
         }
 
         function isObject(obj) {
-          var type = typeof obj;
+          const type = typeof obj;
           return type === 'object' && !!obj;
         }
-        let windowPropsString = ''
-        for (let prop in window) {
-          windowPropsString += prop + ' ';
+        let windowPropsString = '';
+        for (const prop in window) {
+          windowPropsString += `${prop} `;
         }
 
         function iterationCopy(src, skipJunk = true, jsRaw = '') {
-          let target = { constructor: {}, functions: {} };
-          for (let prop in src) {
-            if (src.hasOwnProperty(prop) && (skipJunk || (jsRaw.indexOf(prop) > 0 && windowPropsString.indexOf(prop) < 0))) {
+          const target = { constructor: {}, functions: {} };
+          for (const prop in src) {
+            const sortCondition = src.hasOwnProperty(prop)
+            && (skipJunk || (jsRaw.indexOf(prop) > 0 && windowPropsString.indexOf(prop) < 0));
+
+            if (sortCondition) {
               // if the value is a nested object, recursively copy all it's properties
               if (isFunction(src[prop])) {
                 target.functions[prop] = src[prop].toString();
